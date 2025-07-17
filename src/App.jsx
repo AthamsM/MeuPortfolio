3587fc995c4057e1b8b2417fa15f3f63d0536858
unknown <athamsmenezes@gmail.com>
2025-07-17 18:12:26 -0300

    feat: adicionando responsividade e priorizando o head com um z-40 para os demais nao sobrescreve-lo

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import About from "./pages/About"
 function App() {
   return (
     <div className="bg-radial from-[#1B2735] to-[#090A0F]">
-      <div className="sticky top-0">
+      <div className="sticky top-0 z-40">
         <Header />
       </div>
       <div id="home">
         <Home />
       </div>
-      <div id="projects" className="pt-20">
+      <div id="projects" className="pt-0 pb-0 sm:pt-20 z-10">
         <Projects />
       </div>
-      <div id="about" className="pt-20">
+      <div id="about" className="pt-0 sm:pt-20 z-10">
         <About />
       </div>
     </div>
