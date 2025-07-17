a00bc3f3ec466ac94a0b5f8ae35f55b67c58cf9f
unknown <athamsmenezes@gmail.com>
2025-07-17 18:16:52 -0300

    feat: adicionando texto do botao responsivo

--- src/componentes/Button.jsx
+++ src/componentes/Button.jsx
@@ -1,7 +1,7 @@
 function Button(props){
     return(
         <div className="flex items-center justify-center border border-white rounded-lg w-50 h-8 hover:scale-[1.05]">
-            <a href={props.href} className="text-white text-xl font-kanit" download={props.download}> {props.nome}</a>
+            <a href={props.href} className="text-white text-base sm:text-xl font-kanit" download={props.download}> {props.nome}</a>
         </div>
     )
 }
