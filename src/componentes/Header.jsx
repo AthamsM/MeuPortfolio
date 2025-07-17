31d4a67a5efb98f74a08973346d5404cb36c989a
unknown <athamsmenezes@gmail.com>
2025-07-17 18:30:48 -0300

    fix: adicionando os enderecamentos/href faltantes

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -14,9 +14,9 @@ function Header() {
                 <button onClick={() => scrollToSection("about")} className="text-xl text-white hover:scale-[1.05]">Sobre-mim</button>
             </div>
             <div className="flex gap-4">
-                <a href="" className="text-4xl"><BiLogoGithub className="text-white hover:scale-[1.05]"/></a>
-                <a href="" className="text-4xl"><BiLogoLinkedinSquare className="text-white hover:scale-[1.05]"/></a>
-                <a href="" className="text-4xl"><BiLogoGmail className="text-white hover:scale-[1.05]"/></a>
+                <a href="https://github.com/AthamsM/" className="text-4xl"><BiLogoGithub className="text-white hover:scale-[1.05]"/></a>
+                <a href="https://www.linkedin.com/in/athams-337346274/" className="text-4xl"><BiLogoLinkedinSquare className="text-white hover:scale-[1.05]"/></a>
+                <a href="mailto:athamsmenezes@gmail.com" className="text-4xl"><BiLogoGmail className="text-white hover:scale-[1.05]"/></a>
             </div>
         </div>
     )
