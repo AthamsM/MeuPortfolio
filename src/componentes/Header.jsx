537b6cf39e6d64e6982a58c9084c4a87d7a47138
unknown <athamsmenezes@gmail.com>
2025-07-17 18:19:30 -0300

    feat: responsividade na header do portfolio

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -7,8 +7,8 @@ function Header() {
         document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
     };
     return (
-        <div className="p-10 h-[80px] flex items-center justify-between bg-radial from-[#1B2735] to-[#090A0F]">
-            <div className="flex gap-4 font-kanit">
+        <div className="p-10 h-[80px] flex items-center justify-center sm:justify-between bg-radial from-[#1B2735] to-[#090A0F]">
+            <div className="hidden sm:flex gap-4 font-kanit">
                 <button onClick={() => scrollToSection("home")} className="text-xl text-white hover:scale-[1.05]">Início</button>
                 <button onClick={() => scrollToSection("projects")} className="text-xl text-white hover:scale-[1.05]">Projetos</button>
                 <button onClick={() => scrollToSection("about")} className="text-xl text-white hover:scale-[1.05]">Sobre-mim</button>
